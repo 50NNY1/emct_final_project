3ca00a11520ad7a3db04d527551b95e5419c3732
Sonny Holland <sonnyholland@sonnys-mbp.eduroam.student>
2022-11-11 21:38:20 +0000

    first steps

--- main.cpp
+++ main.cpp
@@ -1,5 +1,34 @@
-#include <iostream>
-
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+#include <functional>  // for function
+#include <iostream>  // for basic_ostream::operator<<, operator<<, endl, basic_ostream, basic_ostream<>::__ostream_type, cout, ostream
+#include <string>    // for string, basic_string, allocator
+#include <vector>    // for vector
+ 
+#include "ftxui/component/captured_mouse.hpp"      // for ftxui
+#include "ftxui/component/component.hpp"           // for Menu
+#include "ftxui/component/component_options.hpp"   // for MenuOption
+#include "ftxui/component/screen_interactive.hpp"  // for ScreenInteractive
+ 
+int main(int argc, const char* argv[]) {
+  using namespace ftxui;
+  auto screen = ScreenInteractive::TerminalOutput();
+ 
+  std::vector<std::string> entries = {
+      "ch1",
+      "ch2",
+      "ch3",
+      "ch4",
+      "ch5",
+      "ch6",
+      "ch7",
+      "ch8",
+  };
+  int selected = 0;
+ 
+  MenuOption option;
+  option.on_enter = screen.ExitLoopClosure();
+  auto menu = Menu(&entries, &selected, &option);
+ 
+  screen.Loop(menu);
+ 
+  std::cout << "Selected element = " << selected << std::endl;
 }
