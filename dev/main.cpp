9f64c15f8df22f839cd9a44015ca3644fd7dec53
Sonny Holland <sonnyholland@sonnys-mbp.home>
2023-03-01 21:48:02 +0000

    beginning code, source: tutorial

--- dev/main.cpp
+++ dev/main.cpp
@@ -1,64 +1,46 @@
-#include <iostream>
-#include <curses.h>
-#include <stdio.h>
+#include <ncurses.h>
 #include <string>
-#include <stdlib.h>
-#include "menu.h"
-#include "grid.h"
-#include <panel.h>
 
-int main()
+#include "Editor.h"
+
+using namespace std;
+
+void curses_init()
 {
-  initscr();
-  cbreak();
-  noecho();    // user cant draw to screen
-  curs_set(0); // cursor invisible
-
-  if (!has_colors())
-  {
-    return -1;
-  }
-  start_color();
-  init_pair(1, COLOR_WHITE, COLOR_BLUE);
-
-  int yMax, xMax;
-  getmaxyx(stdscr, yMax, xMax);
-
-  WINDOW *my_wins[2];
-  PANEL *my_panels[2];
-  my_wins[0] = newwin(yMax / 2, xMax / 2, yMax / 4, xMax / 4);                   // menu bar window
-  my_wins[1] = newwin((yMax / 2) + 1, (xMax / 2) + 1, (yMax / 4) - 1, xMax / 4); // grid window
-
-  my_panels[0] = new_panel(my_wins[0]);
-  my_panels[1] = new_panel(my_wins[1]);
-  update_panels();
-  std::string menu1[] = {"open", "save", "exit"};
-  std::string menu2[] = {"ch1", "ch2", "ch3", "ch4", "ch5", "ch6", "ch7", "ch8"};
-  std::string menu3[] = {"test1", "test2"};
-  Menu menus[3] = {
-      Menu("File", 'a', menu1, 3),
-      Menu("Scene", 's', menu2, 8),
-      Menu("Pattern", 'd', menu3, 2),
-  };
-  MenuBar menubar(my_wins[0], menus, 3);
-  menubar.draw();
-  Grid grid(my_wins[0], 10, 10);
-  grid.print_grid();
-
-  int ch;
-  while (ch = wgetch(my_wins[0]))
-  {
-    menubar.handleTrigger(ch);
-    menubar.draw();
-  }
-  int ch1;
-  while (ch1 = wgetch(my_wins[1]))
-  {
-    grid.handleTrigger(ch1);
-    grid.draw();
-  }
-  update_panels();
-
-  endwin();
-  return 0;
+    initscr();                      // Start ncurses mode
+    noecho();                       // Don't echo keystrokes
+    cbreak();                       // Disable line buffering
+    keypad(stdscr, true);           // Enable special keys to be recorded
 }
+
+int main(int argc, char* argv[])
+{
+    Editor ed;
+    string fn = "";
+    if(argc > 1)
+    {
+        fn = argv[1];               // Set the filename
+        ed = Editor(fn);
+    }
+    else
+    {
+        ed = Editor();
+    }
+
+    curses_init();                  // Initialize ncurses
+
+    while(ed.getMode() != 'x')
+    {
+        ed.updateStatus();
+        ed.printStatusLine();
+        ed.printBuff();
+        int input = getch();        // Blocking until input
+        ed.handleInput(input);
+    }
+
+
+    refresh();                      // Refresh display
+    endwin();                       // End ncurses mode
+    return 0;
+}
+
