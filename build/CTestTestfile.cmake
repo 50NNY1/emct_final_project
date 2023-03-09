3a93789044e686ab1087823799277ad20ed7423f
Sonny Holland <sonnyholland@172-10-23-211.lightspeed.clmasc.sbcglobal.net>
2023-03-09 17:23:15 +0000

    had to revert to beginning tutorial code

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/sonnyholland/Documents/gs/emct_final_project/dev
+# Build directory: /Users/sonnyholland/Documents/gs/emct_final_project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
